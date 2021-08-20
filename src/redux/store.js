8c1db6d468bb791e12d972dcaf4129d6112b84cf
Aung Kaung Myat <aungkaungmyat2596@gmail.com>
2021-08-20 10:48:28 +0630

    Logger fixed

--- src/redux/store.js
+++ src/redux/store.js
@@ -2,13 +2,15 @@ import { createStore, applyMiddleware } from 'redux';
 import { persistStore } from 'redux-persist';
 import createSagaMiddleware from 'redux-saga';
 import rootSaga from './root.saga';
-import logger from 'redux-logger';
+
+// For development
+// import logger from 'redux-logger';
 
 import rootReducer from './root-reducer';
 
 const sagaMiddleware = createSagaMiddleware();
 
-const middlewares = [logger, sagaMiddleware];
+const middlewares = [sagaMiddleware];
 
 export const store = createStore(rootReducer, applyMiddleware(...middlewares));
 
